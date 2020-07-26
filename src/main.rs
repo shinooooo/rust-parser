793e3cb1f115d6f457cc402639c014a6fce49a10
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2020-07-26 16:22:47 +0900

    define Loc & Annot data type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+impl Loc {
+    fn merge(&self, other: &Loc) -> Loc {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, other.1))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Annot<T> {
+    value: T,
+    loc: Loc,
+}
+
+impl<T> Annot<T> {
+    fn new(value: T, loc: Loc) -> Self {
+        Self { value, oc }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
