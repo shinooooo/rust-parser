32e7298a7f1906eeab09b0944b3d07910973c49a
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2020-07-28 22:14:17 +0900

    add lex fn

--- src/main.rs
+++ src/main.rs
@@ -84,4 +84,38 @@ impl LexError {
     }
 }
 
+fn lex(input: &str) -> Result<Vec<Token>, LexError> {
+    let mut tokens = Vec::new();
+
+    let input = input.as_bytes();
+
+    let mut pos = 0;
+
+    macro_rules! lex_a_token {
+        ($lexer: expr) => {{
+            let (tok, p) = $lexer?;
+            tokens.push(tok);
+            pos = p;
+        }};
+    }
+    while pos < input.len() {
+        match input[pos] {
+            b'0'...b'9' => lex_a_token!(lex_number(input, pos)),
+            b'+' => lex_a_token!(lex_plus(input, pos)),
+            b'-' => lex_a_token!(lex_minus(input, pos)),
+            b'*' => lex_a_token!(lex_asterisk(input, pos)),
+            b'/' => lex_a_token!(lex_slash(input, pos)),
+            b'(' => lex_a_token!(lex_lparen(input, pos)),
+            b')' => lex_a_token!(lex_rparen(input, pos)),
+
+            b' ' | b'\n' | b'\t' => {
+                let ((), p) = skip_spaces(input, pos)?;
+                pos = p;
+            }
+            b => return Err(LexError::invalid_char(b as char, Loc(pos, pos + 1))),
+        }
+    }
+    Ok(token)
+}
+
 fn main() {}
